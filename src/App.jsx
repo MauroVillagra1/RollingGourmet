bc8f54e15d97e7de73b479daad5ab7b9f3c8bbd3
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-16 02:36:51 -0300

    Creation of the Product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import 'bootstrap/dist/css/bootstrap.css';
+import CardProduct from "./components/view/product/CardProduct"
 function App() {
 
   return (
     <>
-      
+    <CardProduct></CardProduct>
     </>
   )
 }
