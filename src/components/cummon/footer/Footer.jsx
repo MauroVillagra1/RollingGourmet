bef6f4eb6c70b3b56c3f0d9b8570de027b1c21ed
jonytellodev <jonathantello.dev@gmail.com>
2023-09-16 01:22:57 -0300

    responsive structure added

--- src/components/cummon/footer/Footer.jsx
+++ src/components/cummon/footer/Footer.jsx
@@ -1,19 +1,35 @@
-import React from "react";
-import "./Footer.css";
 import { Container } from "react-bootstrap";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <>
-    <Container className="footerPosition">
-      <div>
-        <h1 className="footerFontFamilyBold">footer Under Construction</h1>
-        <p className="footerFontFamilyNormal">come back soon...</p>
+      <div className="footerPosition footerBackground container-fluid">
+        <div className="row text-center footerFontColor">
+          <div className="separationLine"></div>
+
+          <div className="col-md-3 col-sm-6 col-12 p-2">
+            <img src="" alt="imagen hamburguesa" />
+          </div>
+
+          <div className="col-md-3 col-sm-6 col-12 p-2">
+            <h4>Enlaces</h4>
+          </div>
+
+          <div className="col-md-3 col-sm-6 col-12 p-2">
+            <h4>Informacion</h4>
+          </div>
+
+          <div className="col-md-3 col-sm-6 col-12 p-2">
+            <h4>Horarios</h4>
+          </div>
+        </div>
       </div>
-      <div>
-        <p className="footerFontFamilyNormal">Copyryght 2023 - GrupoTres Developers</p>
-      </div>
-    </Container>
+        <div className="copyStyleGeneral">
+          <p className="footerFontFamilyNormal copyStyleText mt-2">
+            &copy; 2023 - GrupoTres Developers - All rights reserved
+          </p>
+        </div>
     </>
   );
 };
