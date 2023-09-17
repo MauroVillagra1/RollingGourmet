780ae145cd5c2b443ce30c1ee95d1ddefe8036a9
¨mago¨ <martingramajo08@gmail.com>
2023-09-17 12:15:39 -0300

    Agrego la ruta en app.jsx de Administrator/user

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Administrator from "./components/view/administrator/Administrator";
+import AdministratorProduct from "./components/view/administrator/administrator-product/AdministratorProduct";
+import AdministratorUser from "./components/view/administrator/administrator-user/AdministratorUser";
 
 function App() {
   return (
@@ -8,6 +10,16 @@ function App() {
       <Routes>
         <Route exact path="/" element={<h1>Hello world</h1>}></Route>
         <Route exact path="/administrator" element={<Administrator />}></Route>
+        <Route
+          exact
+          path="/administrator/products"
+          element={<AdministratorProduct />}
+        ></Route>
+        <Route
+          exact
+          path="/administrator/users"
+          element={<AdministratorUser />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
