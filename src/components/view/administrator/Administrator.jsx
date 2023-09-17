0bb057df8da213620df4320b27c9a5701eb27b4b
¨mago¨ <martingramajo08@gmail.com>
2023-09-17 10:27:28 -0300

    agrego la view Administrator

--- src/components/view/administrator/Administrator.jsx
+++ src/components/view/administrator/Administrator.jsx
@@ -1,8 +1,24 @@
+import "./Administrator.css";
+import Button from "react-bootstrap/Button";
+
 const Administrator = () => {
   return (
-    <div className="bg-danger">
-      <h1> page admin</h1>
-    </div>
+    <section className="bg-admin-page center">
+      <div>
+        <h1 className="text-color-title text-center pb-5">Administrator</h1>
+        <div className="d-flex flex-column">
+          <Button className="my-2 btn-admin text-white border-0">
+            <span>User Management</span>
+          </Button>
+          <Button className="my-2 btn-admin text-white border-0">
+            <span>Product Management</span>
+          </Button>
+          <Button className="my-2 btn-admin text-white border-0">
+            <span>Orders Management</span>
+          </Button>
+        </div>
+      </div>
+    </section>
   );
 };
 
