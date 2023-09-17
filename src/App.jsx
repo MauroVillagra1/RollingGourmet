bde0878f8b0e6311cddcca8221168b58eede9ff1
¨mago¨ <martingramajo08@gmail.com>
2023-09-17 09:57:07 -0300

    agrego la config inicial de react router dom y visual la page de inicio y la page de administrator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Administrator from "./components/view/administrator/Administrator";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<h1>Hello world</h1>}></Route>
+        <Route exact path="/administrator" element={<Administrator />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/view/administrator/Administrator.jsx
@@ -0,0 +1,9 @@
+const Administrator = () => {
+  return (
+    <div className="bg-danger">
+      <h1> page admin</h1>
+    </div>
+  );
+};
+
+export default Administrator;
