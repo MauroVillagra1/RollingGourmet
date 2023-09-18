0a3eaea63c28e879b50b982838b95995aa60a896
EmiTorres93 <emilianat1992@gmail.com>
2023-09-18 17:31:20 -0300

    Comienzo maquetado de la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,46 @@
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Administrator from "./components/view/administrator/Administrator";
 import AdministratorProduct from "./components/view/administrator/administrator-product/AdministratorProduct";
 import AdministratorUser from "./components/view/administrator/administrator-user/AdministratorUser";
 import AdministratorOrder from "./components/view/administrator/administrator-order/AdministratorOrder";
-import Home from "./components/view/home/Home"
+import Home from "./components/view/home/Home";
+import ProductDetail from "./components/view/productdetail/ProductDetail";
 function App() {
   return (
     <>
-    {/* <CreateProduct></CreateProduct> */}
-    {/* <Order></Order> */}
-    {/* <CardProduct></CardProduct> */}
-    {/* <CardOrder></CardOrder> */}
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home></Home>}></Route>
-        <Route exact path="/administrator" element={<Administrator />}></Route>
-        <Route
-          exact
-          path="/administrator/products"
-          element={<AdministratorProduct />}
-        ></Route>
-        <Route
-          exact
-          path="/administrator/users"
-          element={<AdministratorUser />}
-        ></Route>
-        <Route
-          exact
-          path="/administrator/orders"
-          element={<AdministratorOrder />}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+      {/* <CreateProduct></CreateProduct> */}
+      {/* <Order></Order> */}
+      {/* <CardProduct></CardProduct> */}
+      {/* <CardOrder></CardOrder> */}
+      <ProductDetail></ProductDetail>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home></Home>}></Route>
+          <Route
+            exact
+            path="/administrator"
+            element={<Administrator />}
+          ></Route>
+          <Route
+            exact
+            path="/administrator/products"
+            element={<AdministratorProduct />}
+          ></Route>
+          <Route
+            exact
+            path="/administrator/users"
+            element={<AdministratorUser />}
+          ></Route>
+          <Route
+            exact
+            path="/administrator/orders"
+            element={<AdministratorOrder />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>*/}
     </>
-  )
-
-    
+  );
 }
 
 export default App;
--- src/components/view/productdetail/ProductDetail.jsx
+++ src/components/view/productdetail/ProductDetail.jsx
@@ -1,7 +1,66 @@
-import "./detail.css";
+import "./ProductDetail.css";
+import { Card } from "react-bootstrap";
 
 const ProductDetail = () => {
-  return <div className="detail"></div>;
+  return (
+    <div className="detail d-flex flex-column justify-content-center align-items-center">
+      <div className="container my-5 d-flex flex-column justify-content-center align-items-center">
+        <Card
+          style={{ width: "70rem", height: "25rem" }}
+          className="d-flex flex-row card_title my-3"
+        >
+          <Card.Img
+            style={{ width: "30rem" }}
+            variant="top"
+            src="https://images.pexels.com/photos/4243302/pexels-photo-4243302.jpeg"
+          />
+
+          <Card.Body>
+            <Card.Title className="title fw-bold fs-1 mb-4 pb-2 text-light">
+              Product Name
+            </Card.Title>
+            <Card.Text className="title fw-bold fs-2 mb-4 pb-2 text-light">
+              $ Precio
+            </Card.Text>
+            <div class="container mt-5 text-center fw-bold">
+              <div class="row">
+                <div class="col ">
+                  <h2 className="title fw-bold fs-2 mb-4 pb-2 text-light">
+                    Column
+                  </h2>
+                  <p className="text text-center">Text Description</p>
+                </div>
+                <div class="col">
+                  <h2 className="title fw-bold fs-2 mb-4 pb-2 text-light">
+                    Column
+                  </h2>
+                  <p className="text text-center">Text Description</p>
+                </div>
+                <div class="col">
+                  {" "}
+                  <h2 className="title fw-bold fs-2 mb-4 pb-2 text-light">
+                    Column
+                  </h2>
+                  <p className="text text-center">Text Description</p>
+                </div>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+        <Card
+          style={{ width: "70rem", height: "12rem" }}
+          className="d-flex card_title"
+        >
+          <Card.Title className="p-2 title fw-bold fs-1 text-light">
+            Description
+          </Card.Title>
+          <Card.Body className="text">
+            This is some text within a card body.
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetail;
