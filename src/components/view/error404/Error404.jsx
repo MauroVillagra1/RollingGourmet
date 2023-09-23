30d3ef5c795a576d6b4ee2b64eca190069a5be9e
[LucasA] <[lucas.anachuri@gmail.com]>
2023-09-22 21:59:56 -0300

    agrego visual de error 404

---
+++ src/components/view/error404/Error404.jsx
@@ -0,0 +1,17 @@
+import { Button } from 'react-bootstrap';
+
+
+
+const Error404 = () => {
+    return (
+        <section className="mainSection text-center">
+            
+            <div>
+            <Button variant='success' >Volver al inicio</Button>
+
+            </div>
+        </section>
+    );
+};
+
+export default Error404;
