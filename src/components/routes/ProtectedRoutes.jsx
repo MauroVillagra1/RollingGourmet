bc08b2815398418b5cc413fde027be1a6c5e65d0
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-23 18:49:40 -0300

    work in app and route

--- src/components/routes/ProtectedRoutes.jsx
+++ src/components/routes/ProtectedRoutes.jsx
@@ -4,15 +4,19 @@ import CreateProduct from "../view/administrator/create/CreateProduct";
 import EditProduct from "../view/administrator/edit/EditProduct";
 import AdministratorOrder from "../view/administrator/administrator-order/AdministratorOrder";
 import AdministratorUser from "../view/administrator/administrator-user/AdministratorUser";
-
+import AdministratorProduct from "../view/administrator/administrator-product/AdministratorProduct"
 const ProtectedRoutes = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Administrator />}></Route>
-      <Route exact path="/create" element={<CreateProduct />}></Route>
-      <Route exact path="/edit/:id" element={<EditProduct />}></Route>
+      <Route exact path="/product/create" element={<CreateProduct />}></Route>
+      <Route exact path="/product/edit/:id" element={<EditProduct />}></Route>
       <Route exact path="/order" element={<AdministratorOrder />}></Route>
       <Route exact path="/user" element={<AdministratorUser />}></Route>
+      <Route exact path="/product" element={<AdministratorProduct />}></Route>
+
+     
+
     </Routes>
   );
 };
