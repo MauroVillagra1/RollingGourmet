2f962cce080db5cbf6246cab26f6af3f3f28a518
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-25 20:37:54 -0300

    Protection of my account routes

---
+++ src/components/routes/my-account-routes/ProtectedRoutes_MyAccount.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import MyAccount from "../../view/my-account/MyAccount";
+import OrderHome from "../../view/order/orderHome/OrderHome";
+
+const ProtectedRoutes_MyAccount = () => {
+  return (
+    <Routes>
+      <Route exact path="/" element={<MyAccount></MyAccount>}></Route>
+      <Route exact path="/orders" element={<OrderHome></OrderHome>}></Route>
+      
+
+    </Routes>
+  );
+};
+
+export default ProtectedRoutes_MyAccount;
