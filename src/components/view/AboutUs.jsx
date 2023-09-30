53125c303906dda5643d4ee453d1725abbdfa0ec
Mauro Villagra <maurovillagra257@gmail.com>
2023-09-30 00:50:31 -0300

    modify the card programmer, and a little about us

--- src/components/view/AboutUs.jsx
+++ src/components/view/AboutUs.jsx
@@ -3,6 +3,57 @@ import "./programmer/CardProgrammer.css";
 import { Container } from "react-bootstrap";
 
 const AboutUs = () => {
+  const Programmer = [{
+    Name: "Martin Gramajo",
+    Image: "https://res.cloudinary.com/dhe7vivfw/image/upload/v1696036878/Rolling%20Gourmet/IMG%20PROGRAMMER/303fa6eb-40d2-43b7-9973-f88c30effb5f_omb25b.jpg",
+    Role: "Teach Leader",
+    Years: "29",
+    Email: "martingramajo08@gmail.com",
+    LinkInstagram: "https://www.instagram.com/_mago.jpg/",
+    LinkGithub: "https://github.com/MartinGramajo",
+    LinkLinkedin: "https://www.linkedin.com/in/martin-gramajo-20740a221/"
+  },
+  {
+    Name: "Mauro Villagra",
+    Image: "https://res.cloudinary.com/dhe7vivfw/image/upload/v1696037198/Rolling%20Gourmet/IMG%20PROGRAMMER/90ebd780a48390b673bc34e771f82f93_gfqa3j.jpg",
+    Role: "Scrum Master",
+    Years: "21",
+    Email: "maurovillagra257@gmail.com",
+    LinkInstagram: "https://www.instagram.com/mauro_villagra1/",
+    LinkGithub: "https://github.com/MauroVillagra1",
+    LinkLinkedin: "https://www.linkedin.com/in/mauro-villagra-59433a253/"
+  },
+  {
+    Name: "Emiliana Torres",
+    Image: "https://res.cloudinary.com/dhe7vivfw/image/upload/v1696029648/Rolling%20Gourmet/IMG%20PROGRAMMER/image_25_tt5x94.png",
+    Role: "Collaborating member",
+    Years: "29",
+    Email: "emi.mtorresbq@gmail.com",
+    LinkInstagram: "https://www.instagram.com/emiliana.mtorres/",
+    LinkGithub: "https://github.com/EmiTorres93",
+    LinkLinkedin: "https://www.linkedin.com/in/emiliana-maria-078811197/"
+  },
+  {
+    Name: "Jonathan Emmanuel Tello Carrasco",
+    Image: "https://res.cloudinary.com/dhe7vivfw/image/upload/v1696036898/Rolling%20Gourmet/IMG%20PROGRAMMER/bd5f5710-b864-4aac-9815-37add5674904_ypvaag.jpg",
+    Role: "Collaborating member",
+    Years: "33",
+    Email: "jonathantello.dev@gmail.com",
+    LinkInstagram: "https://www.instagram.com/jonytelloc/",
+    LinkGithub: "https://github.com/jonytellodev",
+    LinkLinkedin: ""
+  },
+  {
+    Name: "Lucas Eduardo Anachuri",
+    Image: "https://res.cloudinary.com/dhe7vivfw/image/upload/v1696036876/Rolling%20Gourmet/IMG%20PROGRAMMER/8dd6a1f1-4352-478c-99ba-8a04a470e1bd_umbac1.jpg",
+    Role: "Collaborating member",
+    Years: "34",
+    Email: "lucas.anachuri@gmail.com",
+    LinkInstagram: "https://www.instagram.com/lucasgyej",
+    LinkGithub: "https://github.com/lucasgt",
+    LinkLinkedin: "https://www.linkedin.com/in/lucas-anachuri/"
+  }
+  ]
   return (
     <>
       <div className="aboutUs container-fluid w-100">
@@ -19,9 +70,12 @@ const AboutUs = () => {
             its users.
           </p>
         </div>
-        <div className="container-fluid w-75">
-          <CardProgrammer></CardProgrammer>
+        <div  className="w-100 justify-content-center d-flex">
+        <div>
+          <CardProgrammer Programmer={Programmer}></CardProgrammer>
         </div>
+        </div>
+       
       </div>
     </>
   );
